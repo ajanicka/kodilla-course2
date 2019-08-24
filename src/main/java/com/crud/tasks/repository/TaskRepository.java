388590a36442297bf6fdaf0fa1ecd4df41751a79
ajanicka <janickanna@gmail.com>
2019-08-24 23:54:51 +0200

    getTask part

---
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -0,0 +1,16 @@
+package com.crud.tasks.repository;
+
+import com.crud.tasks.domain.Task;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+public interface TaskRepository extends CrudRepository<Task, Long> {
+    @Override
+    List<Task> findAll();
+
+    Optional<Task> findById(Long id);
+}
