c6857cf4783a33a8bfbcb7d053b297ff0b324fcc
ajanicka <janickanna@gmail.com>
2019-08-25 22:28:47 +0200

    19.3

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -13,4 +13,7 @@ public interface TaskRepository extends CrudRepository<Task, Long> {
     List<Task> findAll();
 
     Optional<Task> findById(Long id);
+
+    @Override
+    Task save(Task task);
 }
