2b1d280ac483d6e51d5bd7ba3fc62544e984b609
ajanicka <janickanna@gmail.com>
2019-09-29 22:52:36 +0200

    create Task

--- src/main/java/com/crud/tasks/domain/AttachmentByType.java
+++ src/main/java/com/crud/tasks/domain/AttachmentByType.java
@@ -5,20 +5,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloBoardDto {
-    @JsonProperty("name")
-    private String name;
-
-    @JsonProperty("id")
-    private String id;
-
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
+public class AttachmentByType {
+    @JsonProperty("trello")
+    @Autowired
+    Trello trello;
 }
--- src/main/java/com/crud/tasks/domain/Badges.java
+++ src/main/java/com/crud/tasks/domain/Badges.java
@@ -5,20 +5,17 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloBoardDto {
-    @JsonProperty("name")
-    private String name;
+public class Badges {
+    @JsonProperty("votes")
+    private int votes;
 
-    @JsonProperty("id")
-    private String id;
-
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
+    @JsonProperty("attachmentsByType")
+    @Autowired
+    AttachmentByType attachments;
 }
